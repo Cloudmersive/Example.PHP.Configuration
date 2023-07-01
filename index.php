eb543a3624ab6ebce3abc5b30974ee6f7872dedc
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2023-07-01 12:59:21 -0700

    Added

--- index.php
+++ index.php
@@ -1,42 +1,24 @@
 <?php
-
-if(isset($_FILES['image']))
-{
-
-$file_name = $_FILES['image']['name'];   
-$temp_file_location = $_FILES['image']['tmp_name']; 
-
-
-
-
-// Step 1 - Scan for viruses with Cloudmersive Virus Scan
-
 require_once(__DIR__ . '/vendor/autoload.php');
 
 // Configure API key authorization: Apikey
-$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'API-KEY-HERE');
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'YOUR_API_KEY');
+// Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+// $config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKeyPrefix('Apikey', 'Bearer');
 
-
-
-$apiInstance = new Swagger\Client\Api\ImageOcrApi(
-    new GuzzleHttp\Client(  array('verify'          => false)  ),
+$apiInstance = new Swagger\Client\Api\OrchestratorApi(
+    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+    // This is optional, `GuzzleHttp\Client` will be used as default.
+    new GuzzleHttp\Client(),
     $config
 );
-$input_file = $temp_file_location; // \SplFileObject | Input file to perform the operation on.
+$request = new \Swagger\Client\Model\HttpOrchestrationRequest(); // \Swagger\Client\Model\HttpOrchestrationRequest | 
 
-$recognition_mode = "Advanced";
-$language = "ENG";
-$preprocessing = "Auto";
-
-$result = $apiInstance->imageOcrPost($input_file, $recognition_mode, $language, $preprocessing);
-
-
-
-file_put_contents("php://output", $result);
+try {
+    $result = $apiInstance->orchestratorHttpSimple($request);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling OrchestratorApi->orchestratorHttpSimple: ', $e->getMessage(), PHP_EOL;
 }
-?>
 
-<form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">         
-	<input type="file" name="image" />
-	<input type="submit"/>
-</form>      
+?>
